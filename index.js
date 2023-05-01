57bb16fe985d040e45af5cf4f04c7ff743ce7e4e
vishalicious213 <vishalicious213@gmail.com>
2023-05-01 16:39:55 -0700

    added event listneer on button. logging text from input field

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("shopping list")
+const inputField = document.getElementById("input-field")
+const addButton = document.getElementById("add-button")
+
+// ⬇️ EVENT LISTENERS ⬇️
+
+addButton.addEventListener("click", function() {
+    let inputValue = inputField.value
+    console.log(inputValue)
+})
