9e4bf07389480c3e520e070f6d87e934e184292f
vishalicious213 <vishalicious213@gmail.com>
2023-05-01 21:09:50 -0700

    imported initializeApp and getDatabase from Firebase

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
 const inputField = document.getElementById("input-field")
 const addButton = document.getElementById("add-button")
 
